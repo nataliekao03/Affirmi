b8e580f420dbcb797a9ad8e9d5ec616d1e7b873c
Jimmy Vu <jimmyvu1357@gmail.com>
2024-04-27 16:46:55 -0700

    Added firebase config

---
+++ affirmi/src/firebaseConfig.js
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth/web-extension';
+
+// Connect app to Firebase services
+const firebaseConfig = {
+  apiKey: "AIzaSyBoZllS9BMob0Z7ynJwXTjIUE1pbz6reS0",
+  authDomain: "affirmi.firebaseapp.com",
+  projectId: "affirmi",
+  storageBucket: "affirmi.appspot.com",
+  messagingSenderId: "270116631270",
+  appId: "1:270116631270:web:9f32b787c09426cc8e4127",
+  measurementId: "G-ZLZPK9VVCX"
+};
+
+// Create a Firebase app instance and get the authentication service (auth)
+// auth variable provides access to Firebase authentication functionality
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
